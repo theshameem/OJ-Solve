6a2657c166f4901ef2664a0f4fb25de0d1161091
theshameem <theshameem.alam@gmail.com>
2021-01-30 11:10:51 +0600

    Meet In the middle

--- Contest/A.cpp
+++ Contest/A.cpp
@@ -4,7 +4,6 @@ using namespace std;
 typedef long long           ll;
 typedef vector<int>         vi;
 typedef vector<ll>          vll;
-
 #define ff                  first
 #define sf                  scanf
 #define pf                  printf
@@ -12,7 +11,7 @@ typedef vector<ll>          vll;
 #define ppb                 pop_back
 #define pb                  push_back
 #define MP                  make_pair
-#define MAXN                10000
+#define MAXN                1e18
 #define MOD                 1000000007
 #define inf                 0x3f3f3f3f
 #define PI                  (2.0*acos(0.0))
@@ -37,14 +36,71 @@ typedef vector<ll>          vll;
 #define siiii(a, b, c, d)   scanf("%d %d %d %d", &a, &b, &c, &d)
 #define sllll(a, b, c, d)   scanf("%lld %lld %lld %lld", &a, &b, &c, &d)
 #define vout(v)             for(int i = 0; i < v.size(); i++) {cout << v[i]; if(i < v.size() - 1) cout << ' '; else cout << endl;}
+#define eps					1e-9
 
+ll Fibs[100], fib_idx;
+vector<ll> genA, genB;
+
+void genFibs(){
+	Fibs[0] = 2;
+	Fibs[1] = 3;
+	for(int i = 2; i < 86 ; i++){
+		fib_idx = (ll)i + 1;
+		Fibs[i] = Fibs[i - 1] + Fibs[i - 2];
+	}
+}
+
+void call(int pos, int ed, int genX, ll made){
+	if(pos == ed){
+		if(genX == 1) genA.push_back(made);
+		else genB.push_back(made);
+		return;
+	}
+
+	call(pos + 1, ed, genX, made);
+	if(made <= ((long double)MAXN / Fibs[pos]) + eps){
+		call(pos, ed, genX, made * Fibs[pos]);
+	}
+}
+
+void solve(ll a, ll b){
+	ll ans = 0, val = 0;
+
+	FOR(i, 0, genA.size()){
+		if(genA[i] > b) break;
+		ll lo = 0, hi = genB.size() - 1;
+		while(lo <= hi){
+			ll mid = (lo + hi) / 2;
+			if(genA[i] <= (b / genB[mid]) + eps){
+				val = max(val, genA[i] * genB[mid]);
+				lo = mid + 1;
+			} else {
+				hi = mid - 1;
+			}
+		}
+		ans = max(ans, val);
+		val = 0;
+	}
+	if(ans < a || ans == 0) ans = -1; 
+	pf("%lld\n", ans);
+}
 
 int main(){
     #ifndef ONLINE_JUDGE
         double start = clock(); READ(); WRITE();
     #endif
-        
+        genFibs();
+        call(0, 5, 1, 1);
+        call(5, fib_idx, 2, 1);
 
+        sort(all(genA)); unq(genA);
+        sort(all(genB)); unq(genB);
+
+        int t; si(t);
+        while(t--){
+        	ll a, b; sll(a, b);
+        	solve(a, b);
+        }
 
     #ifndef ONLINE_JUDGE
         printf("\n>>Runtime: %.10fs\n", (double)(clock() - start) / CLOCKS_PER_SEC);
